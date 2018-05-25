0d9b1dd8e0afb50e4da81f73c9e0f10504d5f3b3
Simon Baev <simonbaev@gmail.com>
2018-05-25 17:35:03 -0400

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	//--
+	head: {
+		title: "GSW ComElec",
+		meta: [
+			{ charset: "utf-8" },
+			{ name: "viewport", content: "width=device-width, initial-scale=1" },
+			{ hid: "description", name: "description", content: "GSW Committee Election Helper" }
+		],
+		link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+	},
+	//--
+	loading: { color: "#3B8070" },
+	//--
+	css: ["bootswatch/dist/yeti/bootstrap.min.css"],
+	//--
+	modules: [["bootstrap-vue/nuxt", { css: false }]],
+	//--
+	build: {
+		vendor: ["axios"],
+		extend(config, { isDev, isClient }) {
+			if (isDev && isClient) {
+				config.module.rules.push({
+					enforce: "pre",
+					test: /\.(js|vue)$/,
+					loader: "eslint-loader",
+					exclude: /(node_modules)/
+				});
+			}
+		}
+		// postcss: [
+		// 	require('precss')()
+		// ]
+	}
+};
---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+	<section class="container">
+		<div class="py-5">
+			<h2 class="subtitle">
+				GSW Committee Election Helper
+			</h2>
+			<hr>
+			<email-handler/>
+
+		</div>
+	</section>
+</template>
+
+<script>
+import emailHandler from "~/components/emailHandler";
+export default {
+	components: {
+		emailHandler
+	}
+};
+</script>
+
+<style>
+.container {
+	min-height: 100vh;
+	display: flex;
+	justify-content: center;
+	/* text-align: center; */
+}
+
+.subtitle {
+	font-weight: 300;
+	font-size: 42px;
+	color: #526488;
+	word-spacing: 5px;
+	padding-bottom: 15px;
+}
+
+</style>
