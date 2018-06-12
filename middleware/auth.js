caf449471d40245fa37e609b26e1c9cc090a7b1e
Simon Baev <simonbaev@gmail.com>
2018-06-12 15:56:24 -0400

    authentication process refactoring

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,11 @@
-export default function({ store, error }) {
+export default function({ store, error, redirect }) {
 	if (!store.state.authenticated) {
+		redirect('/login');
+		/*
 		error({
 			message: "Access denied",
 			statusCode: 403
 		});
+		 */
 	}
 }
