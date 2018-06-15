ae56291010fd2bf5abdbbe5f04d863c2f6d2c027
Simon Baev <simonbaev@gmail.com>
2018-06-15 13:29:50 -0400

    P6

--- pages/admin.vue
+++ pages/admin.vue
@@ -21,8 +21,7 @@ export default {
 	middleware: "auth",
 	async asyncData({ error }) {
 		try {
-			console.log('-----------',process.server);
-			const { data } = await axios.get(`/api/service/list`);
+			const { data } = await axios.get(`${process.server ? process.env.baseUrl : ''}/api/service/list`);
 			let summaryItems = data.map(e => ({
 				title: e.title,
 				first: 1,
