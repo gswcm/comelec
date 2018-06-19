1c8e30d7f4a9907315087b9303373a87edbe48d8
Simon Baev <simonbaev@gmail.com>
2018-06-19 19:05:25 -0400

    UI tweaks in modifications.vue

--- components/admin/modifications.vue
+++ components/admin/modifications.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<section>
+		<section id="header">
 			<h4 class="my-3">This interface implements 2 features</h4>
 			<ol>
 				<li>
@@ -11,125 +11,133 @@
 				</li>
 			</ol>
 		</section>
-		<b-card no-body class="mt-5">
-			<b-tabs pills card vertical>
-				<b-tab
-					v-for="(a,i) of all"
-					:title="a.committee.title"
-					:key="a.committee.id"
-					title-item-class="rounded"
-					:active="!i"
-					class="h-100">
-					<div class="d-flex flex-column justify-content-between h-100">
-						<div>
-							<div v-if="a.people.length">
-								<h4>Proposed members</h4>
-								<b-table responsive striped :items="a.people" :fields="fields">
+		<section id="main">
+			<b-card no-body class="mt-5">
+				<b-tabs pills card vertical>
+					<b-tab
+						v-for="(a,i) of all"
+						:title="a.committee.title"
+						:key="a.committee.id"
+						title-item-class="rounded"
+						:active="!i"
+						class="h-100">
+						<div class="h-100 d-flex flex-column">
+							<div class="h-100">
+								<div v-if="a.people.length">
+									<h4>Proposed members</h4>
+									<b-table responsive striped :items="a.people" :fields="fields">
+										<template slot="idx" slot-scope="row">
+											{{row.index + 1}}
+										</template>
+										<template slot="email" slot-scope="row">
+											<a :href="`mailto:${row.value}`" target="_blank">{{row.value}}</a>
+										</template>
+										<template slot="x" slot-scope="row">
+											<font-awesome-icon v-if="row.value" :icon="['fas', 'check']"/>
+										</template>
+									</b-table>
+								</div>
+								<b-alert v-else variant="light" show>
+									This committee doesn't have any proposed members
+								</b-alert>
+							</div>
+							<div>
+								<b-btn variant="info" v-b-modal.addMore class="d-block px-3 ml-auto rounded" @click="renderModal(a.committee)">
+									Add more...
+								</b-btn>
+							</div>
+						</div>
+					</b-tab>
+				</b-tabs>
+				<b-modal
+					centered size="lg"
+					no-fade
+					header-bg-variant="dark"
+					header-text-variant="light"
+					ok-title="Submit"
+					ok-variant="info"
+					@ok="processModal"
+					no-close-on-esc
+					id="addMore">
+					<div slot="modal-title">
+						<h5 v-html="modalTitle"/>
+					</div>
+					<div class="p-3 modal-sections">
+						<section id="instructions">
+							<h4>Instructions</h4>
+							<ol>
+								<li>
+									Start typing the <strong>official name</strong> (a.k.a. HR record) of GSW employee <strong>OR</strong> name of the <strong>School / Department</strong> select from the list of options.
+									<b-alert show variant="light" class="mt-2 p-3">
+										<strong>Hint</strong>: If you are not sure about spelling, type all variants separated by a space, e.g. <i>brian bryan</i>
+									</b-alert>
+								</li>
+								<li>
+									Check <strong>ex-officio</strong> mark (if needed), and click on <font-awesome-icon :icon="['fas', 'plus']" transform="down-2"/> to add to the list of proposed additions.
+								</li>
+								<li>
+									Submit modifications by clicking "<strong>Add</strong>" button or simply close the form to discard changes.
+								</li>
+							</ol>
+						</section>
+						<section id="search">
+							<h4>
+								Search...
+							</h4>
+							<b-row align-v="center">
+								<b-col cols="">
+									<no-ssr>
+										<v-autocomplete
+											:items="people"
+											:v-model="person"
+											:get-label="getLabel"
+											:component-item="template"
+											:auto-select-one-item="false"
+											@change="debouncer"
+											@item-selected="itemSelected"
+											:input-attrs="{
+												placeholder: 'Start entering person\'s name or department...'
+											}"/>
+									</no-ssr>
+								</b-col>
+								<b-col cols="auto pl-0">
+									<b-btn variant="info" class="rounded" @click="addToList" :disabled="!person">
+										<font-awesome-icon :icon="['fas', 'plus']"/>
+										Add
+									</b-btn>
+								</b-col>
+							</b-row>
+						</section>
+						<section id="result">
+							<div v-if="modalData && modalData.people && modalData.people.length">
+								<h4>
+									Records to be added
+								</h4>
+								<b-table
+									bordered
+									responsive
+									head-variant=""
+									:items="modalData.people"
+									:fields="modalTableFields">
 									<template slot="idx" slot-scope="row">
 										{{row.index + 1}}
 									</template>
-									<template slot="email" slot-scope="row">
-										<a :href="`mailto:${row.value}`" target="_blank">{{row.value}}</a>
-									</template>
 									<template slot="x" slot-scope="row">
-										<font-awesome-icon v-if="row.value" :icon="['fas', 'check']"/>
+										<b-form-checkbox :id="`ex-officio-flag-${row.index}`" v-model="row.item.x"/>
+									</template>
+									<template slot="action" slot-scope="row">
+										<b-btn variant="link" class="p-0">
+											<font-awesome-icon :icon="['far', 'trash-alt']"/>
+										</b-btn>
 									</template>
 								</b-table>
 							</div>
-							<b-alert v-else variant="light" show>
-								This committee doesn't have any proposed members
-							</b-alert>
-						</div>
-						<div>
-							<b-btn variant="info" v-b-modal.addMore class="d-block px-3 ml-auto rounded" @click="renderModal(a.committee)">
-								Add more...
-							</b-btn>
-						</div>
+							<h4>No records added yet...</h4>
+						</section>
 					</div>
-				</b-tab>
-			</b-tabs>
-			<b-modal
-				centered size="lg"
-				no-fade
-				header-bg-variant="dark"
-				header-text-variant="light"
-				ok-title="Submit"
-				ok-variant="info"
-				@ok="processModal"
-				no-close-on-esc
-				id="addMore">
-				<div slot="modal-title">
-					<h5 class="modal-title" v-html="modalTitle"></h5>
-				</div>
-				<div class="p-3">
-					<section class="mt-3 mb-4">
-						<h4>Instructions</h4>
-						<ol class="mt-3">
-							<li>
-								Start typing the <strong>official name</strong> (a.k.a. HR record) of GSW employee <strong>OR</strong> name of the <strong>School / Department</strong> select from the list of options.
-								<b-alert show variant="light" class="mt-2 p-3">
-									<strong>Hint</strong>: If you are not sure about spelling, type all variants separated by a space, e.g. <i>brian bryan</i>
-								</b-alert>
-							</li>
-							<li>
-								Check <strong>ex-officio</strong> mark (if needed), and click on <font-awesome-icon :icon="['fas', 'plus']" transform="down-2"/> to add to the list of proposed additions.
-							</li>
-							<li>
-								Submit modifications by clicking "<strong>Add</strong>" button or simply close the form to discard changes.
-							</li>
-						</ol>
-					</section>
-					<hr class="my-4">
-					<b-row align-v="center">
-						<b-col cols="">
-							<no-ssr>
-								<v-autocomplete
-									:items="people"
-									:v-model="person"
-									:get-label="getLabel"
-									:component-item="template"
-									:auto-select-one-item="false"
-									@change="debouncer"
-									@item-selected="itemSelected"
-									:min-len="3"
-									:input-attrs="{
-										placeholder: 'Start entering the name...'
-									}"/>
-							</no-ssr>
-						</b-col>
-						<b-col cols="auto">
-							<b-btn variant="outline-info" class="rounded" @click="addToList" :disabled="!person">
-								<font-awesome-icon :icon="['fas', 'plus']"/>
-								Add
-							</b-btn>
-						</b-col>
-					</b-row>
-					<section class="result mt-3" v-if="modalData && modalData.people && modalData.people.length">
-						<h4 class="mt-4 mb-3">
-							Proposed additions:
-						</h4>
-						<b-table
-							bordered
-							responsive
-							head-variant=""
-							:items="modalData.people"
-							:fields="modalTableFields">
-							<template slot="idx" slot-scope="row">
-								{{row.index + 1}}
-							</template>
-							<template slot="x" slot-scope="row">
-								<b-form-checkbox :id="`ex-officio-flag-${row.index}`" v-model="row.item.x"/>
-							</template>
-							<template slot="action" slot-scope="row">
-								<b-btn variant="link" class="p-0">
-									<font-awesome-icon :icon="['far', 'trash-alt']"/>
-								</b-btn>
-							</template>
-						</b-table>
-					</section>
-				</div>
-			</b-modal>
-		</b-card>
+				</b-modal>
+			</b-card>
+		</section>
 	</div>
 </template>
 
@@ -212,6 +220,7 @@ export default {
 					x: false
 				});
 			}
+			this.person = null;
 		},
 		itemSelected(item) {
 			this.person = {...item};
@@ -226,6 +235,9 @@ export default {
 			try {
 				const { data } = await axios.get(`/api/directory/find?query=${text || '.'}`);
 				this.people = data;
+				if(!data.length) {
+					this.person = null;
+				}
 			}
 			catch(error) {
 				if(error.response && error.response.data) {
@@ -258,6 +270,11 @@ export default {
 </script>
 
 <style lang="scss">
+	.modal-sections {
+		section {
+			margin-bottom: 1.5rem;
+		}
+	}
 	hr {
 		border: 1px solid #888;
 	}
