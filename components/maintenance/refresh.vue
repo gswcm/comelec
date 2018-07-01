c81dcce2c6172bd67180e911b993c9969c5577d5
Simon Baev <simonbaev@gmail.com>
2018-07-01 14:37:20 -0400

    maintenance. refresh by name added

--- components/maintenance/refresh.vue
+++ components/maintenance/refresh.vue
@@ -3,12 +3,20 @@
 		<p class="card-text">
 			The internal collection of users of the ComElec application needs to be kept synchronized with any changes projected to the GSW employees directory. Each synchronization may take up to 20-25 seconds and needs to be repeated periodically but not more than once per month.
 		</p>
-		<div class="d-flex justify-content-end">
-			<b-btn variant="outline-danger" @click="refresh" :disabled="loading">
-				<font-awesome-icon v-show="loading" :icon="['fas', 'spinner']" spin/>
-				<strong :class="{'ml-2': loading}">Synchronize database</strong>
-			</b-btn>
-		</div>
+		<p>
+			In case when only a few users need to be updated, a list of their comma-separated <strong>last names</strong> needs to be provided in the field below
+		</p>
+		<b-form @submit.prevent="refresh">
+			<div class="d-flex justify-content-between">
+				<div class="flex-grow-1">
+					<b-input id="query" v-model="query" placeholder="Comma-separated list of last names, e.g. weaver, Lee, ronaldinho, Schwarzenegger"/>
+				</div>
+				<b-btn variant="outline-danger" @click="refresh" :disabled="loading" class="ml-3">
+					<font-awesome-icon v-show="loading" :icon="['fas', 'spinner']" spin/>
+					<strong :class="{'ml-2': loading}">Synchronize database</strong>
+				</b-btn>
+			</div>
+		</b-form>
 	</b-card>
 </template>
 
@@ -16,25 +24,37 @@
 import axios from '~/plugins/axios';
 export default {
 	data: () => ({
-		loading: false
+		loading: false,
+		query: ''
 	}),
 	methods: {
 		async refresh() {
 			try {
 				this.loading = true;
-				const { data } = await axios.get('/api/directory/refresh');
+				const { data } = await axios.get(`/api/directory/refresh${this.query ? '?query=' + this.query.toLowerCase() : ''}`);
 				if(data) {
-					this.$noty.success(`Success: ${data.all} records examined, ${data.new} records added, ${data.upd} records modified`);
+					this.$noty.success(`Success: ${data.all} record(s) matched, ${data.new} record(s) added, ${data.upd} record(s) modified`);
 				}
-				this.loading = false;
 			}
 			catch(error) {
 				console.error(error.message);
 				this.$noty.error(error.message);
+			}
+			finally {
+				this.query = '';
 				this.loading = false;
 			}
 		}
 	}
 }
 </script>
+
+<style>
+	#query:focus {
+		box-shadow: none;
+	}
+</style>
+
+
+
 
