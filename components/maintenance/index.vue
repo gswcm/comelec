a07057dacaa50190f264cdc6f213d5b5bacfbfbd
Simon Baev <simonbaev@gmail.com>
2018-07-05 11:29:22 -0400

    typo on maintenance tab fixed

--- components/maintenance/index.vue
+++ components/maintenance/index.vue
@@ -7,7 +7,7 @@
 				header-border-variant="danger"
 				header-text-variant="danger">
 				<p class="card-text">
-					Incorrect use of this interface may result in unstable behavior of the application. Recovery from an archive is possible but does not guarfantee restoring of the most recent changes. Please use with caution.
+					Incorrect use of this interface may result in unstable behavior of the application. Recovery from an archive is possible but does not guarantee restoring of the most recent changes. Please use with caution.
 				</p>
 			</b-card>
 		</section>
